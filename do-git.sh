8c98e22104b1e037d30b31678dd881a64a6a189b
nimphious <nimphious@gmail.com>
2016-03-11 06:13:13 +0000

    test

--- do-git.sh
+++ do-git.sh
@@ -1,3 +1,19 @@
+for i in "$@"
+do
+case $i in
+    -m=*|--message=*)
+    MESSAGE="${i#*=}"
+    shift
+    ;;
+    *)
+    ;;
+esac
+done
+echo "Commit with message = ${MESSAGE}"
+if [[ -n $1 ]]; then
+    echo "Last line of file specified as non-opt/last argument:"
+    tail -1 $1
+fi
 git add .
-git commit -a -m 'Start.'
+git commit -a -m "${MESSAGE}"
 git push -u origin master
