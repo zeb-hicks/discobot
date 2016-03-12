c6a419bea367421fcb298a50d6e3c6e68368ba83
nimphious <nimphious@gmail.com>
2016-03-12 09:44:41 +0000

    Multi-user support for git shell script.

--- git.sh
+++ git.sh
@@ -1,4 +1,6 @@
-echo "Commit with message: \"$@\""
+read -p "Enter your github username: " USER
+read -p "Enter the comment/message for this commit: " MESSAGE
+git remote set-url origin https://$USER@github.com/zeb-hicks/atotycoon.git
 git add .
-git commit -am "$@"
+git commit -am "$MESSAGE"
 git push -u origin master
