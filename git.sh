8f71943e29c27ee6d609a6b999c0313c4f526efb
nimphious <nimphious@gmail.com>
2016-03-12 21:16:28 +0000

    I'm a moron.

--- git.sh
+++ git.sh
@@ -1,5 +1,5 @@
 read -p "Enter your github username: " USER
 read -p "Enter the comment/message for this commit: " MESSAGE
-git remote set-url origin https://$USER@github.com/zeb-hicks/atotycoon.git
+git remote set-url origin https://$USER@github.com/zeb-hicks/discobot.git
 git commit --interactive -m "$MESSAGE"
 git push -u origin master
