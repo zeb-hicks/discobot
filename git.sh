57ef1aa2b5038348f0175c303b9eadc16245509c
nimphious <nimphious@gmail.com>
2016-03-12 09:55:27 +0000

    Interactive commit.

--- git.sh
+++ git.sh
@@ -1,6 +1,5 @@
 read -p "Enter your github username: " USER
 read -p "Enter the comment/message for this commit: " MESSAGE
 git remote set-url origin https://$USER@github.com/zeb-hicks/atotycoon.git
-git add .
-git commit -am "$MESSAGE"
+git commit --interactive -m "$MESSAGE"
 git push -u origin master
